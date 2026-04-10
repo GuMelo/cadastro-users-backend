77c759ed043ad033bcf3b55a1a883b36df6c5d55
Gustavo Corrente <gustavomelo-1@hotmail.com>
2026-04-10 13:56:50 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,65 @@
+import express from 'express'
+import cors from "cors"
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+const app = express()
+app.use(express.json())
+app.use(cors('http://localhost:3000'))
+
+//Add novo usuário
+app.post('/users', async (req, res) => {
+  console.log("LOGs: ", res)
+  await prisma.user.create({
+    data: {
+      name: req.body.nome,
+      email: req.body.email,
+      age: req.body.idade
+    }
+  })
+  res.status(201).json(req.body)
+})
+
+//Busca todos os usuários no banco
+app.get("/users", async (req, res) => {
+  const users = await prisma.user.findMany()
+  res.status(200).json(users)
+})
+
+//Atualiza dados de um  usuário
+app.put('/users/:id', async (req, res) => {
+  console.log("Req >>> ", req)
+  await prisma.user.update({
+    where: {
+      id: req.params.id
+    },
+    data: {
+      name: req.body.nome,
+      email: req.body.email,
+      age: req.body.idade
+    }
+  })
+  res.status(201).json(req.body)
+})
+
+//Exclui um  usuário
+app.delete('/users/:id', async (req, res) => {
+  await prisma.user.delete({
+    where: {
+      id: req.params.id
+    }
+  })
+  res.status(200).json({"message": "Usuário excluído com sucesso!"})
+})
+
+app.listen(3000)
+
+/* 
+  CRIAR API DE USUÁRIOS (CRUD):
+
+    1. Criar usuários
+    2. Listar todos os usuários
+    3. Editar um usuário
+    4. Deletar usuários
+*/
